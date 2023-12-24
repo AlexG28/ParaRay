2e42725bb4a9eaae732a38e85542e23d4be4602a
Alex <alex.gyumushyan@gmail.com>
2023-12-24 15:59:39 -0500

    added diffuse lighting

--- colour.h
+++ colour.h
@@ -5,10 +5,22 @@
 
 #include <iostream>
 
-void write_colour(std::ostream &out, vec3 pixel_colour){
-    out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
-        << static_cast<int>(255.999 * pixel_colour.y()) << ' '
-        << static_cast<int>(255.999 * pixel_colour.z()) << '\n';
+void write_colour(std::ostream &out, vec3 pixel_colour, int samples_per_pixel){
+    auto r = pixel_colour.x();
+    auto g = pixel_colour.y();
+    auto b = pixel_colour.z();
+
+    // Divide the color by the number of samples.
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    // Write the translated [0,255] value of each color component.
+    static const interval intensity(0.000, 0.999);
+    out << static_cast<int>(256 * intensity.clamp(r)) << ' '
+        << static_cast<int>(256 * intensity.clamp(g)) << ' '
+        << static_cast<int>(256 * intensity.clamp(b)) << '\n';
 }
 
 
